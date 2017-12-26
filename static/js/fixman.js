536fca9a3b1e244bdf8c781ed8d82156aca977f7
roengrit moonchai <logon.firstclass@gmail.com>
2017-12-26 20:46:41 +0700

    user name l max 20 char

--- static/js/fixman.js
+++ static/js/fixman.js
@@ -1,5 +1,11 @@
 $(function () {
     $.get( "/prof-name", function( data ) {
-        $( "#name-l,#name-r" ).html( data );
+        if(data.length>0)
+        {
+            $( "#name-l" ).html( data.substring(0,20) );    
+            $( "#name-r" ).html( data  );           
+        }else{
+             $( "#name-l,#name-r" ).html( data );
+        }
       });
 });
