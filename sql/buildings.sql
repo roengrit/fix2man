035cb5fc54a030d2d3da89ab9afd8c9309e23581
roengrit moonchai <logon.firstclass@gmail.com>
2018-01-02 22:18:39 +0700

    add dev data

---
+++ sql/buildings.sql
@@ -0,0 +1,3 @@
+INSERT INTO "buildings"("i_d", "branch_id", "code", "name", "lock", "created_at", "updated_at") VALUES (1, 1, '0001', 'ตึก 1 ', 'f', '2017-12-31 21:54:51+07', '2017-12-31 21:54:55+07');
+INSERT INTO "buildings"("i_d", "branch_id", "code", "name", "lock", "created_at", "updated_at") VALUES (2, 1, '0002', 'ตึกเฉลิมพระเกียรติ', 'f', '2017-12-31 21:55:22+07', '2017-12-31 21:55:25+07');
+INSERT INTO "buildings"("i_d", "branch_id", "code", "name", "lock", "created_at", "updated_at") VALUES (3, 2, '0003', 'ตึกประโมทกะ', 'f', '2018-01-01 00:25:05+07', '2018-01-01 00:25:09+07');
---
+++ sql/status.sql
@@ -0,0 +1,7 @@
+INSERT INTO "status"("i_d", "code", "name", "created_at", "updated_at", "lock", "is_def") VALUES (1, '0001', 'รอดำเนินการ', '2018-01-01 19:42:34+07', '2018-01-01 19:42:38+07', 't', 't');
+INSERT INTO "status"("i_d", "code", "name", "created_at", "updated_at", "lock", "is_def") VALUES (6, '0002', 'กำลังดำเนินการ', '2018-01-02 21:01:04+07', '2018-01-02 21:01:06+07', 't', 'f');
+INSERT INTO "status"("i_d", "code", "name", "created_at", "updated_at", "lock", "is_def") VALUES (12, '0003', 'รออะไหล่', '2018-01-02 22:02:41+07', '2018-01-02 22:02:44+07', 't', 'f');
+INSERT INTO "status"("i_d", "code", "name", "created_at", "updated_at", "lock", "is_def") VALUES (4, '0004', 'ซ่อมสำเร็จ', '2018-01-02 21:00:27+07', '2018-01-02 21:00:29+07', 't', 'f');
+INSERT INTO "status"("i_d", "code", "name", "created_at", "updated_at", "lock", "is_def") VALUES (8, '0005', 'ซ่อมเสร็จชั่วคราว (รออะไหล่)', '2018-01-02 21:08:18+07', '2018-01-02 21:08:21+07', 't', 'f');
+INSERT INTO "status"("i_d", "code", "name", "created_at", "updated_at", "lock", "is_def") VALUES (11, '0006', 'ซ่อมเสร็จชั่วคราว (สำรองใช้งาน)', '2018-01-02 21:09:09+07', '2018-01-02 21:09:11+07', 't', 'f');
+INSERT INTO "status"("i_d", "code", "name", "created_at", "updated_at", "lock", "is_def") VALUES (7, '0007', 'ยกเลิก', '2018-01-02 21:03:10+07', '2018-01-02 21:03:14+07', 't', 'f');
